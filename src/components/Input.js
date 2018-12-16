5ff98306048794770ce9dfbdc9559a5d25bbb5b6
warbee0712 <warbee0712@outlook.com>
2018-12-16 17:42:06 -0200

    :sparkles: Create Input component

---
+++ src/components/Input.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+import { color, space, fontSize, borderRadius, width } from 'styled-system'
+import { focus } from '../sharedStyles'
+
+const Input = styled.input`
+  border: 1px solid ${props => props.theme.colors.light3};
+  ${borderRadius}
+  ${color}
+  ${fontSize}
+  ${space}
+  ${width}
+  &:hover {
+    box-shadow: ${props => props.theme.shadows.default};
+  }
+  &:focus {
+    ${focus}
+  }
+`
+
+Input.propTypes = {
+  ...borderRadius.propTypes,
+  ...color.propTypes,
+  ...fontSize.propTypes,
+  ...space.propTypes,
+  ...width.propTypes,
+}
+
+Input.defaultProps = {
+  borderRadius: 'default',
+  color: 'dark3',
+  bg: 'white',
+  fontSize: 'xs',
+  px: 'm',
+  py: 's',
+  width: 300,
+}
+
+export default Input
