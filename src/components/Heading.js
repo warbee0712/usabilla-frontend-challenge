bc9123fb564eeb3d9f1797889fcde7639a963ceb
warbee0712 <warbee0712@outlook.com>
2018-12-16 19:26:01 -0200

    :sparkles: Create the Heading component
    It's meant to be used as heading component, but it's just a polymophism of Text using h1 and medium size font

---
+++ src/components/Heading.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+import Text from '../elements/Text'
+
+const Heading = styled(Text).attrs({
+  as: 'h1',
+  fontSize: 'm',
+  lineHeight: 'm',
+})``
+
+export default Heading
