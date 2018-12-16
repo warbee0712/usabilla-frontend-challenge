2ef2bec86032e91a47bec4e8059efc1ee881722e
warbee0712 <warbee0712@outlook.com>
2018-12-16 20:53:41 -0200

    :sparkles: Create PageHeader component

---
+++ src/components/PageHeader.js
@@ -0,0 +1,20 @@
+import React from 'react'
+// Elements
+import Box from '../elements/Box'
+// Components
+import Heading from './Heading'
+
+function PageHeader() {
+  return (
+    <Box px="xl" py="m" position="relative" boxShadow="default">
+      <Heading>
+        <span role="img" aria-label="smiling face with heart-shaped eyes">
+          😍
+        </span>{' '}
+        Dashboard
+      </Heading>
+    </Box>
+  )
+}
+
+export default PageHeader
