fd970390b0b2e3474510926f5bac5faecb363f88
warbee0712 <warbee0712@outlook.com>
2018-12-17 01:21:15 -0200

    :fire: Removing unused css

--- src/sharedStyles.js
+++ src/sharedStyles.js
@@ -5,15 +5,3 @@ export const focus = css`
   box-shadow: ${props => props.theme.shadows.default};
   outline: none;
 `
-
-export const text = css`
-  font-family: ${props => props.theme.fonts.primary};
-  font-size: ${props => props.theme.fontSizes.s};
-  line-height: ${props => props.theme.lineHeights.s};
-`
-
-export const textSmall = css`
-  font-family: ${props => props.theme.fonts.base};
-  font-size: ${props => props.theme.fontSizes.xs};
-  line-height: ${props => props.theme.lineHeights.xs};
-`
